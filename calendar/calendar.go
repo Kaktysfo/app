7ad2281e53515e952f7140895dc42669b691ecfc
KAKTYS <snegovik2312@gmail.com>
2026-03-11 18:20:53 +0300

    methods update

--- calendar/calendar.go
+++ calendar/calendar.go
@@ -7,62 +7,71 @@ import (
 	"github.com/Kaktysfo/app/events"
 )
 
-var calendarEvents = make(map[string]*events.Event)
-var TitleError = errors.New("cобытие с таким именем уже существует")
-var addEventError = errors.New("ошибка добавления события")
-var deleteError = errors.New("ошибка удаления события")
-var showError = errors.New("список событий пуст")
+//var calendarEvents = make(map[string]*events.Event)
+
+var (
+	EventError    = errors.New("cобытие с таким именем уже существует")
+	addEventError = errors.New("ошибка добавления события")
+	deleteError   = errors.New("ошибка удаления события")
+	showError     = errors.New("список событий пуст")
+)
 
 type Calendar struct {
 	calendarEvents map[string]*events.Event
 }
 
 func NewCalendar() *Calendar {
-	return &Calendar{calendarEvents: calendarEvents}
+	return &Calendar{
+		calendarEvents: make(map[string]*events.Event),
+	}
 }
 
-func AddEvent(name, date string) (*events.Event, error) {
+func (c *Calendar) AddEvent(name, date string) (*events.Event, error) {
 	event, err := events.NewEvent(name, date)
 	if err != nil {
-		return &events.Event{}, addEventError
+		return nil, addEventError
 	}
-	calendarEvents[event.ID] = event
-	fmt.Println("Событие добавлено: ", event.Title)
+	c.calendarEvents[event.ID] = event
+	fmt.Println("Событие добавлено:", event.Title)
 	return event, nil
 }
 
-func ShowEvents() error {
-	if len(calendarEvents) == 0 {
+func (c *Calendar) ShowEvents() error {
+	if len(c.calendarEvents) == 0 {
 		return showError
 	}
-	fmt.Println("\nВсе события в календаре: ")
-	fmt.Println("▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼")
-	for _, event := range calendarEvents {
-		fmt.Printf("\nНазвание события:  %s || Дата и время события:  %s ||", event.Title, event.StartAt)
+	for _, event := range c.calendarEvents {
+		fmt.Printf(
+			"\nНазвание события: %s || Дата и время события: %s ||",
+			event.Title,
+			event.StartAt,
+		)
 	}
-	fmt.Println("\n \n▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲")
+
+	fmt.Println("\n▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲")
+
 	return nil
 }
 
-func isTitleExist(title string) error {
-	if _, ok := calendarEvents[title]; !ok {
-		return TitleError
+func (c *Calendar) isEventExist(id string) error {
+	if _, ok := c.calendarEvents[id]; !ok {
+		return EventError
 	}
 	return nil
 }
 
-func DeleteEvent(name string) error {
-	err := isTitleExist(name)
+func (c *Calendar) DeleteEvent(name string) error {
+	err := c.isEventExist(name)
 	if err != nil {
 		return deleteError
 	}
-	delete(calendarEvents, name)
+	delete(c.calendarEvents, name)
 	fmt.Println("Успешно удалено!", name)
 	return nil
 }
 
-func EditEvent(id, newTitle, dateStr string) error {
-	e, exists := calendarEvents[id]
+func (c *Calendar) EditEvent(id, newTitle, dateStr string) error {
+	e, exists := c.calendarEvents[id]
 	if !exists {
 		return errors.New("не удалось найти событие")
 	}
--- main.go
+++ main.go
@@ -9,25 +9,25 @@ import (
 func main() {
 	c := calendar.NewCalendar()
 
-	event1, err1 := calendar.AddEvent("Meeting", "2025/06/12")
+	event1, err1 := c.AddEvent("Meeting", "2025/06/12")
 	if err1 != nil {
 		fmt.Println("Error:", err1)
 	} else {
 		fmt.Println(event1.Title, "added")
 	}
-	event2, err2 := calendar.AddEvent("One more meeting", "2025/06/12")
+	event2, err2 := c.AddEvent("One more meeting", "2025/06/12")
 	if err2 != nil {
 		fmt.Println("Error:", err2)
 	} else {
 		fmt.Println(event2.Title, "added")
 	}
-	err := calendar.EditEvent(event2.ID, "Call", "2025/06/12 16:50")
+	err := c.EditEvent(event2.ID, "Call", "2025/06/12 16:50")
 	if err != nil {
 		fmt.Println("Error:", err)
 	} else {
 		fmt.Println("Event updated")
 	}
-	errShow2 := calendar.ShowEvents()
+	errShow2 := c.ShowEvents()
 	if errShow2 != nil {
 		fmt.Println(errShow2)
 	}
